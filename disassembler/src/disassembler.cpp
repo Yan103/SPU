1f7aca1431f0ad3efc4a2a88fafa0936ad0fb521
Yan103 <101476599+Yan103@users.noreply.github.com>
2024-10-23 04:16:31 +0300

    Add files via upload

---
+++ disassembler/src/disassembler.cpp
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <string.h>
+
+#include <stdlib.h>
+
+#include "/home/projects/SPU/cpu/include/Default.h"
+#include "/home/projects/SPU/cpu/include/ReturnCodes.h"
+
+static const char* INPUT_FILENAME  = "/home/projects/SPU/assembler_code.txt";
+static const char* OUTPUT_FILENAME = "/home/projects/SPU/disassembler_code.txt";
+
+int main() {
+    FILE* input_filename = fopen(INPUT_FILENAME, "rb");
+
+    if (!input_filename) {
+        printf(RED("Error occured while opening file\n"));
+
+        return BAD_FILE;
+    }
+
+    int cmds[CMDS_SIZE] = {};
+
+    fread(cmds, sizeof(int), CMDS_SIZE, input_filename);
+    fclose(input_filename);
+
+    FILE* output_filename = fopen(OUTPUT_FILENAME, "w");
+
+    if (!output_filename) {
+        printf(RED("Error occured while opening file\n"));
+
+        return BAD_FILE;
+    }
+
+    int i = 0, DoFlag = 1;
+
+    while (i != CMDS_SIZE && DoFlag == 1) {
+        switch (cmds[i]) {
+            case PUSH:
+                fprintf(output_filename, "%s ", "push");
+                fprintf(output_filename, "%d\n", cmds[i+1]);
+                i += 2;
+                break;
+
+            case PUSHR:
+                fprintf(output_filename, "%s ", "pushr");
+                fprintf(output_filename, "%d\n", cmds[i+1]);
+                i += 2;
+                break;
+
+            case POPR:
+                fprintf(output_filename, "%s\n", "popr");
+                i++;
+                break;
+
+            case ADD:
+                fprintf(output_filename, "%s\n", "add");
+                i++;
+                break;
+
+            case SUB:
+                fprintf(output_filename, "%s\n", "sub");
+                i++;
+                break;
+
+            case DIV:
+                fprintf(output_filename, "%s\n", "div");
+                i++;
+                break;
+
+            case MLT:
+                fprintf(output_filename, "%s\n", "mlt");
+                i++;
+                break;
+
+            case IN:
+                fprintf(output_filename, "%s\n", "in");
+                i++;
+                break;
+
+            case OUT:
+                fprintf(output_filename, "%s\n", "out");
+                i++;
+                break;
+
+            case JMP:
+                fprintf(output_filename, "%s ", "jmp");
+                fprintf(output_filename, "%d", cmds[i++]);
+                fprintf(output_filename, "%s\n", ":");
+                i++;
+                break;
+
+            case JNE:
+                fprintf(output_filename, "%s\n", "jne");
+                fprintf(output_filename, "%d", cmds[i++]);
+                fprintf(output_filename, "%s\n", ":");
+                i++;
+                break;
+
+            case HLT:
+                fprintf(output_filename, "%s", "hlt");
+                DoFlag = 0;
+                break;
+
+            default:
+                fprintf(output_filename, "%d", cmds[i++]);
+                fprintf(output_filename, "%s\n", ":");
+                break;
+            }
+    }
+
+    fclose(output_filename);
+
+    printf(GREEN("DISASSEMBLING END!\n"));
+
+    return SUCCESS;
+}
